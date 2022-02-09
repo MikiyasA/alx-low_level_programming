4d1eb54dffe6a03750f4936cf12cdf86650d94b6
MikiyasA <mikiyasad86@gmail.com>
2022-02-09 04:08:36 -0800

    To commit mandatory task of 0x1C project

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+The Method that returns the perimeter of the island described in grid:
+
+grid is a list of list of integers:
+    0 represents a water zone
+    1 represents a land zone
+    One cell is a square with side length 1
+    Grid cells are connected horizontally/vertically (not diagonally).
+"""
+def island_perimeter(grid):
+    """ returns the perimeter of the island described in grid
+    """
+    perimeter = 0
+
+    noRow = len(grid)
+
+    if grid != []:
+        noCol = len(grid[0])
+
+    for a in range(noRow):
+        for b in range(noCol):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == noRow or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == noCol or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return (perimeter)
